c188e06707e210cb3ab1c97d4c8cbd9d46c12a6b
xue <xue@Aula-B4-PC-117>
2015-01-30 11:55:54 +0000

    werlcome to the game

--- moneda/src/moneda/moneda.java
+++ moneda/src/moneda/moneda.java
@@ -1,33 +1,34 @@
 package moneda;
 
 public class moneda {
-	//prueba 
+	// prueba
 	private int euros;
 	public static int M2 = 2;
 	public static int M1 = 1;
-	
-	public static void main(String[] args){
+	public int moneda;
+
+	public static void main(String[] args) {
 		moneda d = new moneda(21);
-		d.printDescomposicionMonedas(); 
+		d.printDescomposicionMonedas();
 	}
-	public moneda(int euros){
-		this.euros = euros; 
+
+	public moneda(int euros) {
+		this.euros = euros;
 	}
-	
-	public void printDescomposicionMonedas(){
-		
+
+	public void printDescomposicionMonedas() {
+
 		int billete10 = euros / 810;
 		int resto = euros - billetes10 * 10;
-		resto = resto - billetes5 * 5; 
+		resto = resto - billetes5 * 5;
 		int monedas = resto / M2;
-		resto = resto - monedas2 * 2; 
-		
+		resto = resto - monedas2 * 2;
+
 		System.out.print("Billetes 10: " + billetes10);
 		System.out.print("Billetes 5: " + billetes5);
 		System.out.print("Monedas 2: " + monedas2);
 		System.out.print("Monedas 1: " + resto);
 
 	}
-	
 
 }
